2d049ca1103f9fc702872948be5cbe20758215c9
Karabo <Maimane23@hotmail.com>
2024-08-14 23:04:57 +0200

    Initial commit: NodeJS Basics

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 
 const server = http.createServer((req,res) => {
     const url = req.url;
-
     const method = req.method;
+
     if(url === '/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
@@ -14,11 +14,25 @@ const server = http.createServer((req,res) => {
     }
 
     if(url === '/message' && method === 'POST'){
-        fs.writeFileSync('message.txt', 'Dummy Text');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        const body = [];
+
+        req.on('data', (chunk) => {
+            console.log('chunk data: ', chunk);
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            message = parsedBody.split('=')[1];
+            console.log('parsed body data: ', parsedBody);
+            fs.writeFileSync('message.txt', message, (err) => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
     }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
